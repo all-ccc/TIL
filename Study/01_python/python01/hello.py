8f3bb036565280a0e1f4fb4125efb5cbc898b6f7
차민주 <ckalswn007@naver.com>
2024-01-15 17:05:53 +0900

    240115 학습 추가

---
+++ Study/01_python/python01/hello.py
@@ -0,0 +1,5 @@
+print('hello, world!')
+
+# vscode 설정 변경
+# ctrl shift p => open user settings(JSON)
+# vscode에서 tap 눌렀을 때 몇 번 스페이스바 되는지 설정(mm에 있음)
---
+++ Study/01_python/python01/python_ws_1_1/ws_1_1.py
@@ -0,0 +1,13 @@
+name_value = '홍길동'
+age_value = 20
+name = '이름 : '
+age = '나이 : '
+s1 = '학생 정보를 출력합니다.'
+s2 = '각각의 정보는 다음과 같습니다'
+s3 = '출력 완료'
+
+print(s1)
+print(s2)
+print(name, name_value)
+print(age, age_value)
+print(s3)
