c178417699596edff2b7255bf4718539e55261b7
차민주 <ckalswn007@naver.com>
2024-01-15 16:31:39 +0900

    파일 수정

--- Algorithm/SWEA/Unsolved/1959.py
+++ Algorithm/SWEA/Unsolved/1959.py
@@ -1,14 +1,17 @@
 # 1959 두 개의 숫자열
 
-test_num = int(input())
+test_num = int(input()) # 10
 
 sum = 0
 
-max = 0
 
-for n in range(1, test_num+1):
+for n in range(1, test_num + 1):
 
+    max = 0
     a, b = map(int, input().split())
-
+    if a < b:
+        list_a = list(map(int, input().split()))
+        list_b = list(map(int, input().split()))
+        
 
     print('#%d %d' %(n, max))
