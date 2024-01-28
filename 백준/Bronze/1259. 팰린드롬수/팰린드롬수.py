d94b8bb43e89c7b721a9de75d807ec54d8afd464
minju <126561415+all-ccc@users.noreply.github.com>
2024-01-28 23:01:26 +0900

    [Bronze I] Title: 팰린드롬수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -0,0 +1,11 @@
+import sys
+# sys.stdin = open('input.txt')
+
+while True:
+    number = input()
+    if number == '0':
+        break
+    if number == number[::-1]:
+        print('yes')
+    else:
+        print('no')
