cfae2147709410d16dc63378a7bfecbda4a90dd6
차민주 <ckalswn007@naver.com>
2024-02-02 17:40:47 +0900

    240202

--- SWEA/1210_ladder_1/sol.py
+++ SWEA/1210_ladder_1/sol.py
@@ -4,9 +4,10 @@ sys.stdin = open('input.txt')
 for test in range(1, 11):
     test_case = int(input())
     ladder = [list(map(int, input().split())) for _ in range(100)]
+    # ladder = [[0] * 100] * 100 -> 이렇게 하면 복제돼서 숫자 하나 바꾸면 다 바뀜
 
-    end_col = ladder[99].index(2) # 마지막 행에서 2가 있는 열을 찾아서 end_col에
-    end_row = 99
+    end_col = ladder[99].index(2) # 마지막 행에서 2가 있는 열을 찾아서 end_col에 5
+    end_row = 95
 
     while end_row != 0:
         # 좌우 중 1이 있으면 그쪽 열로 옮김
@@ -22,13 +23,3 @@ for test in range(1, 11):
             end_row -= 1
             if end_row == 0:
                 print(f'#{test} {end_col}')
-
-
-
-
-
-
-
-
-
-
---
+++ SWEA/16268_풍선팡2/sol.py
@@ -0,0 +1,38 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+
+for test in range(1, T+1):
+    N, M = map(int, input().split())
+    # N : 행의 수 3
+    # M : 열의 수 5
+
+    # 종이 꽃가루 개수 들어간 2차원 리스트
+    data = [[list(map(int, input().split()))] for _ in range(M)]
+
+    #           상  하  좌  우
+    delta_row = [-1, 1, 0, 0]
+    delta_col = [0, 0, -1, 1]
+
+    # N x M 의 위치 하나하나를 돌면서 터지는 꽃가루 수 계산(flower)해서 list에 저장
+    flower_lst = []
+    for x in range(N): # 0 1 2
+        for y in range(M): # 0 1 2 3 4
+            # 방향 당 터지는 꽃가루 수 += 로 더해줌
+            # 처음엔 현재 위치의 꽃가루 값
+            flower = data[x][y]
+            for i in range(4): # 상하좌우 네 가지 방향
+                # 방향 별 x, y 좌표 값
+                next_row = x + delta_row[i]
+                next_col = y + delta_col[i]
+                # next 좌표가 data 안에 위치
+                if 0 > next_row or next_row > N:
+                    flower += data[next_row][next_col]
+                else:   # data 안에 위치하지 않을 때
+                    flower += 0
+                # out of range 뜨는데 data 안에 위치하지 않을 때를 조건 첫번째로 해야 하는지
+
+            flower_lst.append(flower)
+
+    print(f'{test} {max(flower_lst)}')
