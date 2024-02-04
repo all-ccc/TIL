bcd687b57ea76c3a824baa791cc9336707b94bb8
차민주 <ckalswn007@naver.com>
2024-02-05 01:19:49 +0900

    16268_풍선팡2

--- SWEA/16268_풍선팡2/sol.py
+++ SWEA/16268_풍선팡2/sol.py
@@ -20,19 +20,14 @@ for test in range(1, T+1):
     for x in range(N): # 0 1 2
         for y in range(M): # 0 1 2 3 4
             # 방향 당 터지는 꽃가루 수 += 로 더해줌
-            # 처음엔 현재 위치의 꽃가루 값
-            flower = data[x][y]
+            flower = data[x][y]    # 처음엔 현재 위치의 꽃가루 값
             for i in range(4): # 상하좌우 네 가지 방향
                 # 방향 별 x, y 좌표 값
                 next_row = x + delta_row[i]
                 next_col = y + delta_col[i]
                 # next 좌표가 data 안에 위치
-                if 0 > next_row or next_row > N:
+                if 0 <= next_row < N and 0 <= next_col < M:
                     flower += data[next_row][next_col]
-                else:   # data 안에 위치하지 않을 때
-                    continue
-                # out of range 뜨는데 data 안에 위치하지 않을 때를 조건 첫번째로 해야 하는지
-
             flower_lst.append(flower)
 
-    print(f'{test} {max(flower_lst)}')
+    print(f'#{test} {max(flower_lst)}')
