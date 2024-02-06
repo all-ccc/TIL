afc242570bca4b34b40e8943f15e1fb8bfa2b026
차민주 <ckalswn007@naver.com>
2024-02-06 23:24:10 +0900

    240206 회문 문제 추가

--- SWEA/4861_회문/sol.py
+++ SWEA/4861_회문/sol.py
@@ -6,29 +6,27 @@ T = int(input())
 for test in range(1, T + 1):
     # N : 글자의 길이, 글자의 줄 수
     # M : 회문의 길이
-    N, M = map(int, input().split()) # 20 13 -> 8번을 돌아야
+    N, M = map(int, input().split())
     # N x N 크기의 격자
-    data = [list(input().split()) for _ in range(N)]
+    data = [list(input()) for _ in range(N)]
+    # 나 이때까지 input().split() 쓰고 있었음;;
 
     # 가로 단어
     for x in range(N):  # 행
-        for y in range(N-M+1):  # 열
-            row_word = data[x][0][y:y+M]
-            print(row_word)
+        for y in range(N - M + 1):  # 열
+            row_word = data[x][y:y+M]
             if row_word == row_word[::-1]:
-                print(f'#{test} {row_word}')
+                result = ''.join(row_word)
+                print(f'#{test} {result}')
                 break
 
     # 세로 단어
-    col_word = ''
-    for x in range(N):  # 행
-        for y in range(N-M+1):  # 열
+    for x in range(N - M + 1):  # 행
+        for y in range(N):  # 열
+            col_word = ''
             for i in range(M):
-                word = data[i+y][x] # out of range
-                print(word)
-            #     col_word += word
-            #     print(col_word)
-            # if col_word == col_word[::-1]:
-            #     print(f'#{test} {col_word}')
-            #     break
-    # 다시 풀기 ㅎ;
+                word = data[i + x][y]
+                col_word += word
+            if col_word == col_word[::-1]:
+                print(f'#{test} {col_word}')
+                break
