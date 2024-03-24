ca130208495d48dc4c6a60427c5b684edd4e0d26
minju <126561415+all-ccc@users.noreply.github.com>
2024-03-24 18:52:19 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 309 ms, Memory: 70,200 KB -BaekjoonHub

---
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+T = int(input())
+for test in range(1, T+1):
+    N, B = map(int, input().split())    # N: 점원 수, B: 선반 높이
+    clerk = list(map(int, input().split()))
+    min_sum = int(1e9)
+    for j in range(1, N+1):
+        for k in combinations(clerk, j):
+            sum_result = sum(k)
+            if sum_result >= B:  # 선반보다 높으면
+                min_sum = min(min_sum, sum_result)
+    print(f'#{test} {min_sum-B}')
