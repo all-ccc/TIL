7a8eb8dbebd366b9f384e359a761ff31294bb10f
minju <126561415+all-ccc@users.noreply.github.com>
2025-02-12 16:46:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.08 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+   let result = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (result[result.length - 1] !== arr[i]) {
+            result.push(arr[i]);
+        }
+    }
+    
+    return result;
+}
