9f2568b4f161e3a29cbbf7c2e026aef69d2b75e2
minju <126561415+all-ccc@users.noreply.github.com>
2025-02-25 23:50:01 +0900

    [level 3] Title: 여행경로, Time: 76.56 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -3,9 +3,11 @@ function solution(tickets) {
     const visited = Array(len).fill(false);
     let result = [];
     
+    tickets.sort();
+    
     function dfs(route) {
-        if (route.length === len + 1) {
-            result.push(route);
+        if (route.length === len + 1 && result.length === 0) {
+            result = route;
             return;
         }
         
@@ -15,11 +17,11 @@ function solution(tickets) {
             if (!visited[i] && route[route.length - 1] === start) {
                 visited[i] = true;
                 dfs([...route, end]);
-                visited[i] = false;
+                visited[i] = false; // 백트래킹
             }
         }
     }
     dfs(['ICN']);
     
-    return result.sort()[0];
+    return result;
 }
