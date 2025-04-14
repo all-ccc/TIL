c131ee04130430e0bb7bde5061bacfa3e4b50614
minju <126561415+all-ccc@users.noreply.github.com>
2025-04-14 18:13:30 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 40.54 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -0,0 +1,34 @@
+function solution(queue1, queue2) {
+    const queue = [...queue1, ...queue2];
+    const n = queue1.length;
+    const maxCount = n * 4;
+    
+    const totalSum = queue.reduce((a, b) => a + b, 0);
+    
+    if (totalSum % 2 !== 0) return -1;
+    
+    const target = totalSum / 2;
+    
+    let count = 0;
+    let start = 0;
+    let end = n;
+    let temp = queue1.reduce((a, b) => a + b, 0);
+    
+    while (count < maxCount) {
+        if (temp === target) return count;
+        
+        if (temp < target) {
+            temp += queue[end % (n * 2)];
+            end++;
+        } else {
+            temp -= queue[start % (n * 2)];
+            start++;
+        }
+        
+        count++;
+    }
+    
+    return -1;
+}
+
+
